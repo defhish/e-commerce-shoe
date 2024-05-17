f884913f0c7b5266fd1c6ce2c515de85bff803a9
Aditya <adityakumarji74@gmail.com>
2024-05-17 13:04:17 +0530

    Routing done

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage.jsx";
+import ProductDetail from "./Pages/ProductDetail.jsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/productDetail",
+        element: <ProductDetail />,
+      },
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain="dev-ufg3sbrpr0btzdlb.us.auth0.com"
     clientId="ldRSFKTmSMuwJhoaWYmJVzELa2hfsob0"
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
     }}
   >
-    <App />
+    <RouterProvider router={router} />
   </Auth0Provider>
-    
- 
-)
+);
