266fac84010aad088de6582be587ae7e2315ec69
Aditya <adityakumarji74@gmail.com>
2024-05-24 10:14:45 +0530

    product card changed

--- project/src/Pages/LandingPage.jsx
+++ project/src/Pages/LandingPage.jsx
@@ -53,7 +53,15 @@ const LandingPage = () => {
       </div>
 
       <div className="product-list">
-        <h2 style={{ textAlign: "center" }}>
+        <h2
+          style={{
+            textAlign: "center",
+            fontSize: "4rem",
+            fontWeight: "400",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+            marginBottom: "3rem",
+          }}
+        >
           Explore the widest collection of sportswear
         </h2>
         <ProductTab></ProductTab>
