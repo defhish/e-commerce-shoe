440f28de7d35faabd65793fced5bc32f9cfbca55
Meet <91799@Meet>
2024-05-24 15:26:05 +0530

    conflicts resolved

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./common/header/header.jsx";
 import Product_Title from "./components/slider_content/Product_Title.jsx";
+import Landing_Page from "./Pages/LandingPage.jsx";
 import ProductTab from "./components/Prodect_List/ProductTab.jsx";
 import Discount from "./components/Discount/discount.jsx";
 import NewsLetter from "./components/Monthly News Letter/newsLetter.jsx";
@@ -13,7 +14,7 @@ function App() {
     <>
       <Header></Header>
       <Product_Title></Product_Title> 
-      <ProductTab></ProductTab>
+      <Landing_Page></Landing_Page>
       <Discount></Discount>
       <NewsLetter></NewsLetter>
 
--- project/src/Pages/LandingPage.jsx
+++ project/src/Pages/LandingPage.jsx
@@ -1,51 +1,12 @@
 
-import ImageSlider from "../components/slider_content/Product_Title.jsx";
 import ProductTab from "../components/Prodect_List/ProductTab.jsx";
 import Discount from "../components/Discount/discount.jsx";
 import NewsLetter from "../components/Monthly News Letter/newsLetter.jsx";
 
 const LandingPage = () => {
-  const images = [
-    {
-     
-      title: "NIKE 1 SPORTS SHOES",
-      subtitle: "NIKE METCON SHOES",
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio ad laudantium iste tenetur ab asperiores temporibus rerum eius autem eaque. Necessitatibus a ducimus, delectus similique mollitia reiciendis consequatur dolorem? Vitae.",
-    },
-    {
-      title: "NIKE 2  SPORTS SHOES",
-      subtitle: "NIKE METCON SHOES",
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio ad laudantium iste tenetur ab asperiores temporibus rerum eius autem eaque. Necessitatibus a ducimus, delectus similique mollitia reiciendis consequatur dolorem? Vitae.",
-    },
-    {
-     
-      title: "NIKE 3 SPORTS SHOES",
-      subtitle: "NIKE METCON SHOES",
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio ad laudantium iste tenetur ab asperiores temporibus rerum eius autem eaque. Necessitatibus a ducimus, delectus similique mollitia reiciendis consequatur dolorem? Vitae.",
-    },
-    {
-     
-      title: "NIKE 4 SPORTS SHOES",
-      subtitle: "NIKE METCON SHOES",
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio ad laudantium iste tenetur ab asperiores temporibus rerum eius autem eaque. Necessitatibus a ducimus, delectus similique mollitia reiciendis consequatur dolorem? Vitae.",
-    },
-  ];
+  
   return (
     <>
-      <div
-        style={{
-          width: "100%",
-          height: "28px",
-          padding: "0 9%",
-          flex: "1 1 460px",
-        }}
-      >
-        <ImageSlider slides={images}></ImageSlider>
-      </div>
 
       <div className="product-list">
         <h2
