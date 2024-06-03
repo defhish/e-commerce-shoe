77560da98de0972f4c781b1906313b2e5cb729bb
Meet <91799@Meet>
2024-06-03 14:55:10 +0530

    conflicts resolved

---
+++ project/src/Pages/PaymentPage/Arrow_button.jsx
@@ -0,0 +1,13 @@
+import { FaChevronRight } from "react-icons/fa";
+import './Arrow_button.css';
+
+function Arrow_button({text}) {
+    return (
+        <button className="arrow-button">
+            {text}
+            <FaChevronRight id="arrow-icon" />
+        </button>
+    );
+}
+
+export default Arrow_button;
