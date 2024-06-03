b4e06a4e2c77d1d22125fc4f6c43f24a5c1e5a5b
Meet <91799@Meet>
2024-06-03 14:58:11 +0530

    conflicts resolved

--- project/src/Pages/PaymentPage/Arrow_button.jsx
+++ project/src/Pages/PaymentPage/Arrow_button.jsx
@@ -8,12 +8,9 @@ function Arrow_button({text}) {
             <FaChevronRight id="arrow-icon" />
         </button>
     );
-<<<<<<< HEAD
+
 }
 
 export default Arrow_button;
-=======
-};
 
-export default Arrow_button;
->>>>>>> aa271489da518c7ea35b2986c20aa1657da88ffa
+
